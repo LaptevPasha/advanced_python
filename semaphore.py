aa8ac2c270ccff41fbfeaa66ef0272e1ca5f84ec
Pavel Laptev <pavel.laptev@gmail.com>
2019-01-22 00:20:18 +0300

    Homework3: Pavel Laptev

---
+++ semaphore.py
@@ -0,0 +1,26 @@
+import threading
+import os
+
+
+def number_generator(thread_number):
+    global counter
+    while True:
+        semaphore.acquire()
+        if counter < 101:
+            if ((counter % 2) == (thread_number % 2)):
+                print('Thread ' + str(thread_number) + ': ' + str(counter))
+                counter += 1
+        else:
+            os._exit(0)
+        semaphore.release()
+
+
+counter = 0
+
+semaphore = threading.Semaphore()
+
+thread_a = threading.Thread(target=number_generator, args=[0])
+thread_b = threading.Thread(target=number_generator, args=[1])
+
+thread_a.start()
+thread_b.start()
