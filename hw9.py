0af557072006fab0b292c2ec872da78112440731
Pavel Laptev <pavel.laptev@gmail.com>
2019-02-22 23:10:22 +0300

    homework9

---
+++ hw9.py
@@ -0,0 +1,19 @@
+import csv
+from itertools import accumulate
+from itertools import takewhile
+
+
+max_weight = 400
+itemlist = []
+
+with open('items.csv') as csvfile:
+    csvfile.readline()
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        itemlist.append(row)
+
+sorted_list = list(reversed(sorted(itemlist, key=(lambda item: int(item[2]) 
+                                                  / int(item[1])))))
+running_add = takewhile(lambda z: z < max_weight,
+                        (accumulate([int(i[1]) for i in sorted_list])))
+print(sorted_list[0: len(list(running_add))])
