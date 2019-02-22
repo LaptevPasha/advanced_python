b585c9bd258d2e28e6f37595565e806b64c08dbf
Pavel Laptev <pavel.laptev@gmail.com>
2019-02-22 23:25:53 +0300

    codestyle warning fixed

--- hw9.py
+++ hw9.py
@@ -12,7 +12,7 @@ with open('items.csv') as csvfile:
     for row in csvreader:
         itemlist.append(row)
 
-sorted_list = list(reversed(sorted(itemlist, key=(lambda item: int(item[2]) 
+sorted_list = list(reversed(sorted(itemlist, key=(lambda item: int(item[2])
                                                   / int(item[1])))))
 running_add = takewhile(lambda z: z < max_weight,
                         (accumulate([int(i[1]) for i in sorted_list])))
