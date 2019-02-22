546e80bbfec18c5124688b3ff2c969aa032d77ed
Pavel Laptev <pavel.laptev@gmail.com>
2019-02-22 23:49:18 +0300

    pycodestyle warning fixed-2

--- hw9.py
+++ hw9.py
@@ -13,7 +13,7 @@ with open('items.csv') as csvfile:
         itemlist.append(row)
 
 sorted_list = list(reversed(sorted(itemlist, key=(lambda item: int(item[2])
-                                                   / int(item[1])))))
+                                                  / int(item[1])))))
 running_add = takewhile(lambda z: z < max_weight,
                         (accumulate([int(i[1]) for i in sorted_list])))
 print(sorted_list[0: len(list(running_add))])
